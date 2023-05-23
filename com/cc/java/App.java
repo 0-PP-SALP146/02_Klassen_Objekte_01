34c764f7a8d11fef20c5ad3b9ffa5c96bda53847
Serhedo <serhat.alpayci@gmx.de>
2023-05-23 11:01:51 +0200

    Ausgabe über Methoden/direkt

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,20 +4,37 @@ public class App {
     
    public static void main(String[] args) {
 
-        /* Instanziierung (Objekt) Klasse --> Objekt */
-        // Cat -- Referenztyp
-        // cat -- Referenzvariable
+     /* Instanziierung (Objekt) Klasse --> Objekt */
+     // Cat -- Referenztyp
+     // cat -- Referenzvariable
          
-        Cat cat1 = new Cat();
-        output("Blick von außen: " + cat1);
-        output(cat1.tellYourAddress());
+     Cat cat1 = new Cat();
 
-       // App.output("hi");
-        output("--------------------");
+     // Wertzuweisung
+     cat1.name = "Grizabella";
+     cat1.furColor = "white";
+     cat1.age = 29;
+
+     // Ausgabe über Methoden
+     output(cat1.tellYourName());
+     output(cat1.tellYourFurColor());
+     output(Integer.toString(cat1.tellYourAge())); // Typkonvertierung
+
+
+     // App.output("hi");
+     output("--------------------");
     
-        Cat cat2 = new Cat();
-        output("Blick von außen: " + cat2);
-        output(cat2.tellYourAddress());
+     Cat cat2 = new Cat();
+
+     // Wertzuweisung
+     cat2.name = "Alonzo";
+     cat2.furColor = "grey";
+     cat2.age = 35;
+
+     // Ausgabe direkt
+     output(cat2.name);
+     output(cat2.furColor);
+     output(Integer.toString(cat2.age)); // Typkonvertierung
 
    } 
 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,7 +2,26 @@ package com.cc.java;
 
 public class Cat {
     
-public String tellYourAddress() {
+    public String name;
+    public String furColor;
+    public int age;
+
+
+    public String tellYourName() {
+        return this.name;
+    }
+
+    public String tellYourFurColor() {
+        return this.furColor;
+    }
+
+    public int tellYourAge() {
+        return this.age;
+    }
+
+
+    // this --> Instanzvariable // Selbstreferenz
+    public String tellYourAddress() {
     return "Blick von innen: " + this;
 }
 
